458b48c2db076a812c4c8b60a53f84d2f870dc72
ahmed ahmed s <71298128+ahmedsaied223@users.noreply.github.com>
2025-12-14 20:45:02 -0500

    Delete cypress directory

--- cypress/support/command.ts
+++
@@ -1,5 +0,0 @@
-Cypress.Commands.add('login', (username: string, password: string) => {
-  cy.get('#username').type(username);
-  cy.get('#password').type(password);
-  cy.get('#login').click();
-});
